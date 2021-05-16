7c0cd0610b732afa871f269dd0438396854a13fd
enshawali <enshaw@wittyfeed.com>
2021-05-16 12:55:32 +0530

    Declared resourcecomparer globally in test class thats a bad practice

--- app/src/androidTest/java/com/testyourskills/ResourceComparerTest.kt
+++ app/src/androidTest/java/com/testyourskills/ResourceComparerTest.kt
@@ -3,23 +3,39 @@ package com.testyourskills
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 
-class ResourceComparerTest{
-   private val resourceComparer=ResourceComparer()
+class ResourceComparerTest {
+
+    private lateinit var resourceComparer: ResourceComparer
+
+    @Before
+    fun setup() {
+        /*executes before every test case it reduce bolierplate code that creates instatnce*/
+        resourceComparer = ResourceComparer()
+    }
+
+    @After
+    fun teardown() {
+
+    }
 
     @Test
-    fun stringResourceSameAsPassString_returnTrue(){
-        val context=ApplicationProvider.getApplicationContext<Context>()
-        val result=resourceComparer.isEqual(context,R.string.app_name,"TestMySkills")
+    fun stringResourceSameAsPassString_returnTrue() {
+
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context, R.string.app_name, "TestMySkills")
         assertThat(result).isTrue()
     }
 
     @Test
-    fun stringResourceSameAsPassString_returnFalse(){
-        val context=ApplicationProvider.getApplicationContext<Context>()
-        val result=resourceComparer.isEqual(context,R.string.app_name,"Skills")
+    fun stringResourceSameAsPassString_returnFalse() {
+
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context, R.string.app_name, "Skills")
         assertThat(result).isFalse()
     }
 }
